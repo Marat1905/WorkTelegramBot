e8c518f6f79b1e422c597745d52ce9eebf5855e3
Marat <milya010790@mail.ru>
2024-01-23 21:36:23 +0500

    Начальное построение бота

--- WorkTelegramBot.BotConsole/Program.cs
+++ WorkTelegramBot.BotConsole/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace WorkTelegramBot.BotConsole
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Text;
+using Telegram.Bot;
+using WorkTelegramBot.BotConsole.Controllers;
+
+namespace WorkTelegramBot.BotConsole
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static async Task Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // Объект, отвечающий за постоянный жизненный цикл приложения
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services)) // Задаем конфигурацию
+                .UseConsoleLifetime() // Позволяет поддерживать приложение активным в консоли
+                .Build(); // Собираем
+
+            Console.WriteLine("Сервис запущен");
+            // Запускаем сервис
+            await host.RunAsync();
+            Console.WriteLine("Сервис остановлен");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            // Подключаем контроллеры сообщений и кнопок
+            services.AddTransient<DefaultMessageController>();
+            services.AddTransient<VoiceMessageController>();
+            services.AddTransient<TextMessageController>();
+            services.AddTransient<InlineKeyboardController>();
+
+
+            // Регистрируем объект TelegramBotClient c токеном подключения
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("6299377057:AAHaNlY93hdrdQVanTPgmMibgQt41UDidRA"));
+            // Регистрируем постоянно активный сервис бота
+            services.AddHostedService<Bot>();
         }
     }
 }
